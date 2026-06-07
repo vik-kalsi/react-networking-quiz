a60dddbd1a101b0c5f9d514e97ec2db5cb0d742c
vik-kalsi <107356027+vik-kalsi@users.noreply.github.com>
2026-06-07 18:59:37 +0100

    Improving final score by 2 decimal points

--- src/components/TotalScoreComponent.tsx
+++ src/components/TotalScoreComponent.tsx
@@ -12,7 +12,7 @@ function TotalScoreComponent(props:{ finalScore: number}) {
         <>
             <div className="space-y-4">
                 <h1 className="text-4xl">Final Score</h1>
-                <p className="text-3xl">You've scored {props.finalScore} correct answers out of 15 questions ({(props.finalScore/15)*100}%)</p>
+                <p className="text-3xl">You've scored {props.finalScore} correct answers out of 15 questions ({((props.finalScore/15)*100).toFixed(2)}%)</p>
                 <button className="border-2 p-2 text-2xl" onClick={StartAgain}>Start Again</button>
             </div>
         </>
